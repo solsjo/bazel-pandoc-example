7b1ee6d755a0e1c70aa6acb39c10c3ac865c662b
solsjo <oskar.solsjo@gmail.com>
2022-08-17 12:41:31 +0200

    Update pdf_to_image.py

--- pdf_to_image.py
+++ pdf_to_image.py
@@ -2,23 +2,25 @@ from pdf2image import convert_from_path
 
 
 def setup_argparse():
-    return None
+    parser = argparse.ArgumentParser(description='Foo')
+    parser.add_argument('-o', '--output', help='Output file name', default='stdout')
+    requiredNamed = parser.add_argument_group('required named arguments')
+    requiredNamed.add_argument('-i', '--input', help='Input file name', required=True)
+    requiredNamed.add_argument('-f', '--format', help='format', required=True)
+    return parser
 
 
 def main():
     argparse = setup_argparse()
-    #args = arparse.parse()
-    #input = args.input
-    #output = args.output_dir
-    #format = args.format
+    args = arparse.parse()
+    input = args.input
+    output = args.output_dir
+    format = args.format
     # Store Pdf with convert_from_path function
-    #images = convert_from_path(input)
+    images = convert_from_path(input)
  
-    #for i in range(len(images)):
-    #    # Save pages as images in the pdf
-    #    output_path = output + '/' + input.basename() + f'.{format}'
-    #    images[i].save(output_path, format.toupper())
-    pass
+    for i in range(len(images)):
+        images[i].save(output, format.upper())
 
 if __name__ == "__main__":
   main()
