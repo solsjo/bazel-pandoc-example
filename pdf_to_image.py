77f9655e004d738bb7d6facfe049c908339802a9
solsjo <oskar.solsjo@gmail.com>
2022-08-17 09:02:50 +0200

    Create pdf_to_image.py

---
+++ pdf_to_image.py
@@ -0,0 +1,24 @@
+from pdf2image import convert_from_path
+
+
+def setup_argparse():
+    return None
+
+
+def main():
+    argparse = setup_argparse()
+    #args = arparse.parse()
+    #input = args.input
+    #output = args.output_dir
+    #format = args.format
+    # Store Pdf with convert_from_path function
+    #images = convert_from_path(input)
+ 
+    #for i in range(len(images)):
+    #    # Save pages as images in the pdf
+    #    output_path = output + '/' + input.basename() + f'.{format}'
+    #    images[i].save(output_path, format.toupper())
+    pass
+
+if __name__ == "__main__":
+  main()
