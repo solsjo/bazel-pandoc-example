1af1928185761de47cc3da37f615f57493d5db13
solsjo <oskar.solsjo@gmail.com>
2022-09-07 08:11:35 +0200

    fix typo

--- svg2pdf.py
+++ svg2pdf.py
@@ -1,6 +1,8 @@
 from svglib.svglib import svg2rlg
 from reportlab.graphics import renderPDF, renderPM
 
+import sys
+
 if __name__ == "__main__":
     drawing = svg2rlg(sys.argv[1])
     renderPDF.drawToFile(drawing, sys.argv[2])
